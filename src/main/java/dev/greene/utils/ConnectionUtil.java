85ea8178157d642c2a82d616db957f15ce26dde0
ngreeneUT <ngreen11@vols.utk.edu>
2019-10-20 17:21:01 -0400

    changed ConnectionUtil class

--- src/main/java/dev/greene/utils/ConnectionUtil.java
+++ src/main/java/dev/greene/utils/ConnectionUtil.java
@@ -17,25 +17,23 @@ public class ConnectionUtil {
 		
 		
 		try {
-			Properties props = new Properties();
-			File file = new File("C:/Users/oskar/Desktop/1909java/AMWatch/src/main/resources/connection.properties");
-			FileInputStream in = new FileInputStream(file.getAbsolutePath());
-			props.load(in);
+		//	Properties props = new Properties();
+		//	File file = new File("C:/Users/oskar/Desktop/1909java/AMWatch/src/main/resources/connection.properties");
+		//	FileInputStream in = new FileInputStream(file.getAbsolutePath());
+		//	props.load(in);
 			try {
 				Class.forName("org.mariadb.jdbc.Driver");
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			}
-			String conInfo = props.getProperty("conn");
-			Connection conn = DriverManager.getConnection(conInfo);
+			//String conInfo = props.getProperty("conn");
+
+			Connection conn = DriverManager.getConnection("jdbc:mariadb://gnb.cr2qttzvwrif.us-east-1.rds.amazonaws.com:3306/Ankh-MorporkWatch?user=admin&password=password");
+			
 			
 			return conn;
 			
 		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
